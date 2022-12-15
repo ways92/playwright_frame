22e53549922dd2d6d79c484bf5478723cfc95086
Ways Alqorni <wayslos92@gmail.com>
2022-12-15 22:28:08 +0700

    add negative case

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require( '@playwright/test' )
 const { LoginPage } = require( '../pageObject/loginPage' )
+const dataLogin = require( '../fixture/dataLogin.json' )
 
 test.describe( 'Login Test', () => { 
 
@@ -14,15 +15,78 @@ test.describe( 'Login Test', () => {
         
     });
     
-    test( 'login concept POM', async ( { page } ) => {
+    test( 'Login concept POM', async ( { page } ) => {
         const lp = new LoginPage( page )
         await lp.navigateToWeb()
         await lp.fillUsername( 'standard_user' )
         await lp.fillPassword( 'secret_sauce' )
         await lp.clickLogin()
         await lp.waitTime(4000)
-        await lp.verifyLogin()
+        await lp.verifyLogin('Sauce Labs Backpack')
         
-    } );
+    });
+    
+    test( 'Login with empty username', async ( { page } ) => {
+        const lp = new LoginPage( page )
+        await lp.navigateToWeb()
+        await lp.fillUsername()
+        await lp.fillPassword( 'secret_sauce' )
+        await lp.clickLogin()
+        await lp.waitTime(2000)
+        await lp.verifyLogin('Epic sadface: Username is required')
+    });
+    
+    test( 'Login with empty password', async ( { page } ) => {
+        const lp = new LoginPage( page )
+        await lp.navigateToWeb()
+        await lp.fillUsername( 'standard_user' )
+        await lp.fillPassword()
+        await lp.clickLogin()
+        await lp.waitTime(2000)
+        await lp.verifyLogin('Epic sadface: Password is required')
+    });
+    
+    test( 'Login with empty username and password', async ( { page } ) => {
+        const lp = new LoginPage( page )
+        await lp.navigateToWeb()
+        await lp.fillUsername()
+        await lp.fillPassword()
+        await lp.clickLogin()
+        await lp.waitTime(2000)
+        await lp.verifyLogin('Epic sadface: Username is required')
+    });
+    
+    test( 'Login with wrong username', async ( { page } ) => {
+        const lp = new LoginPage( page )
+        await lp.navigateToWeb()
+        await lp.fillUsername('12345678')
+        await lp.fillPassword('secret_sauce')
+        await lp.clickLogin()
+        await lp.waitTime(2000)
+        await lp.verifyLogin('Username and password do not match any user in this service')
+    });
+    
+    test( 'Login with wrong password', async ( { page } ) => {
+        const lp = new LoginPage( page )
+        await lp.navigateToWeb()
+        await lp.fillUsername('standard_user')
+        await lp.fillPassword('qwerty')
+        await lp.clickLogin()
+        await lp.waitTime(2000)
+        await lp.verifyLogin('Username and password do not match any user in this service')
+    });
+    
+    dataLogin.forEach(data => {
+        test( `Login DDT with ${data.case}`, async ( { page } ) => {
+            const lp = new LoginPage( page )
+            await lp.navigateToWeb()
+            await lp.fillUsername(data.username)
+            await lp.fillPassword(data.password)
+            await lp.clickLogin()
+            await lp.waitTime(2000)
+            await lp.verifyLogin(data.verification)
+        });
+        
+    });
     
 });
